1f3bd94940c360a51c28c7624dd9e8e6dabf2f7c
jackkaiser1 <jackkaiser78@gmail.com>
2026-03-30 16:20:28 -0400

    initial commit

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,4 +1,15 @@
+import amqp from "amqplib";
+
 async function main() {
+  const rabbitConnString = "amqp://guest:guest@localhost:5672/";
+  const conn = await amqp.connect(rabbitConnString);
+  console.log("Connection successfull");
+
+  process.on("SIGINT", () => {
+    console.log("Game server is now shutting down");
+    conn.close();
+  });
+
   console.log("Starting Peril server...");
 }
 
